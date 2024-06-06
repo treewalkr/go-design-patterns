c51a40001394462ea5d27ce838c457e1f426ff41
treewalkr <suthep@pinroj.com>
2024-06-07 00:14:45 +0700

    Add select statement pattern

---
+++ concurrency/select_statement/select_statement.go
@@ -0,0 +1,30 @@
+package selectstatement
+
+import (
+	"fmt"
+	"time"
+)
+
+func Example() {
+	ch1 := make(chan string)
+	ch2 := make(chan string)
+
+	go func() {
+		time.Sleep(1 * time.Second)
+		ch1 <- "Message from ch1"
+	}()
+
+	go func() {
+		time.Sleep(2 * time.Second)
+		ch2 <- "Message from ch2"
+	}()
+
+	for i := 0; i < 2; i++ {
+		select {
+		case msg1 := <-ch1:
+			fmt.Println(msg1)
+		case msg2 := <-ch2:
+			fmt.Println(msg2)
+		}
+	}
+}
